65541bcfa0d5cfef09db2262cdc0a47c3ae90df7
Jacquita Adams <adamslaquita24@gmail.com>
2020-07-25 14:15:02 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by MXC Swift on 7/25/20.
+//  Copyright © 2020 MXC Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
